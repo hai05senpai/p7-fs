480ed40f72d1921b8554f85adfc057682d066cb4
hai05senpai <hai.buisongbien@hcmut.edu.vn>
2026-03-15 18:01:39 +0700

    Trang chi tiết công việc - BE

---
+++ controllers/job.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import Job from "../models/job.model";
+import AccountCompany from "../models/account-company.model";
+
+export const detail = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const record = await Job.findOne({
+      _id: id,
+    });
+
+    if(!record){
+      return res.json({
+        code: "error",
+        message: "Công việc không tồn tại",
+      });
+    }
+
+    const jobDetail = {
+      id: record._id,
+      title: record.title,
+      companyName: "",
+      salaryMin: record.salaryMin,
+      salaryMax: record.salaryMax,
+      images: record.images,
+      position: record.position,
+      workingFrom: record.workingFrom,
+      companyAddress: "",
+      technologies: record.technologies,
+      description: record.description,
+      companyLogo: "",
+      companyId: record.companyId,
+      companyModel: "",
+      companyEmployees: "",
+      companyWorkingTime: "",
+      companyWorkOvertime: "",
+    };
+
+    const infoCompany = await AccountCompany.findOne({
+      _id: record.companyId,
+    });
+
+    if(infoCompany){
+      jobDetail.companyName = `${infoCompany.companyName}`;
+      jobDetail.companyAddress = `${infoCompany.address}`;
+      jobDetail.companyLogo = `${infoCompany.logo}`;
+      jobDetail.companyModel = `${infoCompany.companyModel}`;
+      jobDetail.companyEmployees = `${infoCompany.companyEmployees}`;
+      jobDetail.companyWorkingTime = `${infoCompany.workingTime}`;
+      jobDetail.companyWorkOvertime = `${infoCompany.workOvertime}`;
+    }
+
+    res.json({
+      code: "success",
+      message: "Lấy chi tiết công việc thành công",
+      jobDetail: jobDetail,
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Lấy chi tiết công việc thất bại",
+    });
+  }
+}
